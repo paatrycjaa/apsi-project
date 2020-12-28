0edc20e0c620e5fbc325dabb5c44334525b409d6
Wojtek <wrzesien.ww@gmail.com>
2020-12-28 11:20:02 +0100

    Add login page.

--- django_project/innovation_module/urls.py
+++ django_project/innovation_module/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='app/components/login/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('ideas', views.ideas, name='ideas'),
     path('add-idea', views.add_idea, name='add_idea'),
     path('add-opinion/<int:idea_id>/', views.add_opinion, name='add_opinion'),
