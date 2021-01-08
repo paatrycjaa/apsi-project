23ba69d88527e5dc1e29ea6e2cb0f2a1b1ede18b
Wojtek <wrzesien.ww@gmail.com>
2021-01-08 19:00:29 +0100

    Redirect to idea edit.

--- client/app/components/my-ideas-list/myIdeasList.controller.js
+++ client/app/components/my-ideas-list/myIdeasList.controller.js
@@ -1,22 +1,35 @@
 angular.module('appMyIdeasListController', [])
-  .controller('myIdeasListController', ['$scope', '$window', 'myIdeasListService',
-      function($scope, $window, myIdeasListService) {
+  .controller('myIdeasListController', ['$scope', '$window', '$document','myIdeasListService',
+      function($scope, $window, $document, myIdeasListService) {
         
-        $scope.user_ideas = [];
+        $scope.ideas_rest = [];
+        $scope.ideas_to_edit = [];
 
         $scope.init = function() {
           myIdeasListService.getUserIdeas(function(response) {
-            $scope.user_ideas = response.data
-            console.log($scope.user_ideas)
+            for (idea of response.data) {
+              if (idea.fields.status == 'Edycja') {
+                $scope.ideas_to_edit.push(idea);
+              } else {
+                $scope.ideas_rest.push(idea);
+              }
+            }
+            console.log('Ideas to edit');
+            console.log($scope.ideas_to_edit);
+            console.log('Rest of ideas');
+            console.log($scope.ideas_rest);
           });
-        }
 
-        // $scope.ideaVisible = function(idea) {
-        //   return idea.fields.status == 'Oczekujacy'
-        // }
+          
+        }
 
         $scope.openIdeaOpinions = function(id) {
           $window.location.href = `/opinions/${id}/`;
+        }
+
+        $scope.editIdea = function(id) {
+          // $window.location.href = `//${id}/`;
+          console.log('Redirect to idea edit');
         }
       }
 ]);
--- client/app/components/my-ideas-list/myIdeasList.service.js
+++ client/app/components/my-ideas-list/myIdeasList.service.js
@@ -4,11 +4,5 @@ angular.module('appMyIdeasListService', [])
       this.getUserIdeas = function(callback) {
         return $http.get('/ajax/user_ideas/', JSON).then(callback);
       }
-
-      // this.editIdea = function(data, callback) { 
-      //   console.log(data)
-      //   console.log(JSON.stringify(data))
-      //   return $http.post('/ajax/edit_idea/', JSON.stringify(data)).then(callback);
-      // }
     }
   );
