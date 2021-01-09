e4f5b10397bb8b71cc30904b07e81af70f5088c4
marcingajewski14 <44137698+marcingajewski14@users.noreply.github.com>
2021-01-09 15:06:54 +0100

    Update ideaAddition.service.js

--- client/app/components/idea-addition/ideaAddition.service.js
+++ client/app/components/idea-addition/ideaAddition.service.js
@@ -5,15 +5,10 @@ angular.module('appIdeaAdditionService', [])
         return $http.get(`/ajax/get_idea/${id}/`).then(callback);
       }
       this.submitIdea = function(data, callback) { 
-        console.log(data)
-        console.log(JSON.stringify(data))
         return $http.post('/ajax/submit_idea/', JSON.stringify(data)).then(callback) }
 
       this.editIdea = function( data, callback) { 
         var url = '/ajax/edit_idea/'
-        console.log(data)
-        console.log(JSON.stringify(data))
-        console.log(url)
         return $http.post(url, JSON.stringify(data)).then(callback)
       }
     }
